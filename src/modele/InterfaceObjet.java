5772bcee65b92f6132c1fae895cda91d198d17ec
fredericRignault <rignaultfrederic@gmail.com>
2017-04-22 12:40:00 -0400

    Je sais plus

--- src/modele/InterfaceObjet.java
+++ src/modele/InterfaceObjet.java
@@ -91,7 +91,6 @@ public class InterfaceObjet {
 	{
 
 		InterfaceObjet temp = new InterfaceObjet(new TestFields(42,true,41.0f,(short)10,120000,(byte) 18,'a',"bonjour",20.0d));
-		//InterfaceObjet temp = new InterfaceObjet(new Integer(5));
 		return temp;
 	}
 
